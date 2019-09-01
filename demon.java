6e242cbe130c11fcc87f287bf466c43093e2ff94
zouwensheng <2541488464@qq.com>
2019-09-01 10:10:53 +0800

    commit secound by zws

--- demon.java
+++ demon.java
@@ -1,4 +1,11 @@
 public class Student {
     private Integer id;
     private String name;
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
